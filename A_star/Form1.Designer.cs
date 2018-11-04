4be9fa52b8adde34913a9bf90954a846fbb89e50
Karolis Staskevičius <satelistas@gmail.com>
2018-11-04 19:56:13 +0200

    Developement (#1)
    * Initial Form
    
    * Additional project changes
    
    * Extra mazes added

--- A_star/Form1.Designer.cs
+++ A_star/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.advanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.advanceStepToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.actionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.advanceToolStripMenuItem,
+            this.advanceStepToolStripMenuItem,
+            this.runToolStripMenuItem});
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.actionToolStripMenuItem.Text = "Action";
+            // 
+            // advanceToolStripMenuItem
+            // 
+            this.advanceToolStripMenuItem.Name = "advanceToolStripMenuItem";
+            this.advanceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.advanceToolStripMenuItem.Text = "Advance";
+            this.advanceToolStripMenuItem.Click += new System.EventHandler(this.advanceToolStripMenuItem_Click);
+            // 
+            // advanceStepToolStripMenuItem
+            // 
+            this.advanceStepToolStripMenuItem.Name = "advanceStepToolStripMenuItem";
+            this.advanceStepToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.advanceStepToolStripMenuItem.Text = "Advance Step";
+            this.advanceStepToolStripMenuItem.Click += new System.EventHandler(this.advanceStepToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(884, 712);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "A* path search by Karolis Staskevičius";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressEvent);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem advanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem advanceStepToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
     }
 }
 
---
+++ A_star/Square.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace A_star
+{
+	enum SquareType { Normal, Obstacle, Source, Terminal }
+	class Square
+	{
+		public int x { get; private set; }
+		public int y { get; private set; }
+		public int sourcex;
+		public int sourcey;
+
+		public PictureBox background;
+		Label weightLabelShort;
+		Label weightLabelLong;
+		SquareType type;
+		public bool Calculated { get; private set; }
+		int weighttosource;
+		int weighttoterminal;
+		public int step;
+
+		public SquareType Type
+		{
+			get { return type; }
+			set
+			{
+				type = value;
+				sourcex = -1;
+				sourcey = -1;
+				switch(value)
+				{
+					case SquareType.Normal:
+						Calculated = false;
+						background.BackColor = Color.White;
+						weightLabelShort.Text = "";
+						weightLabelLong.Text = "";
+						step = 0;
+						break;
+					case SquareType.Obstacle:
+						background.BackColor = Color.Black;
+						weightLabelShort.Text = "";
+						weightLabelLong.Text = "";
+						step = 0;
+						break;
+					case SquareType.Source:
+						background.BackColor = Color.Blue;
+						weightLabelShort.Text = "S";
+						weightLabelLong.Text = "";
+						step = 1;
+						break;
+					case SquareType.Terminal:
+						background.BackColor = Color.Green;
+						weightLabelShort.Text = "T";
+						weightLabelLong.Text = "";
+						step = 0;
+						break;
+				}
+			}
+		}
+
+		public int Weight
+		{
+			get { return weighttosource + weighttoterminal; }
+		}
+		public int WeightToSource
+		{
+			get { return weighttosource; }
+			set
+			{
+				weighttosource = value;
+				Calculate();
+			}
+		}
+		public int WeightToTerminal
+		{
+			get { return weighttoterminal; }
+			set
+			{
+				weighttoterminal = value;
+				Calculate();
+			}
+		}
+
+
+		public Square(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+			sourcex = -1;
+			sourcey = -1;
+			int xCoord = 50 + (x * 40);
+			int yCoord = 30 + (y * 40);
+
+			background = new PictureBox();
+			background.BackColor = Color.White;
+			background.BorderStyle = BorderStyle.FixedSingle;
+			background.Location = new System.Drawing.Point(xCoord, yCoord);
+			background.Name = String.Format("pictureBox{0}_{1}", xCoord, yCoord);
+			background.Size = new System.Drawing.Size(40, 40);
+			background.TabIndex = 0;
+			background.TabStop = false;
+
+
+			weightLabelShort = new Label();
+			weightLabelShort.Parent = background;
+			weightLabelShort.BackColor = System.Drawing.Color.Transparent;
+			weightLabelShort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			weightLabelShort.Name = String.Format("labelShort{0}_{1}", xCoord, yCoord);
+			weightLabelShort.Size = new System.Drawing.Size(30, 20);
+			weightLabelShort.Location = new System.Drawing.Point(5, 5);
+			weightLabelShort.TabIndex = 1;
+			weightLabelShort.Text = "";
+
+			weightLabelLong = new Label();
+			weightLabelLong.Parent = background;
+			weightLabelLong.BackColor = System.Drawing.Color.Transparent;
+			weightLabelLong.Name = String.Format("labelLong{0}_{1}", xCoord, yCoord);
+			weightLabelLong.Size = new System.Drawing.Size(36, 13);
+			weightLabelLong.Location = new System.Drawing.Point(2, 27);
+			weightLabelLong.TabIndex = 1;
+			weightLabelLong.Text = "";
+		}
+
+		private void Calculate()
+		{
+			if (weighttosource > 0 && weighttoterminal > 0)
+			{
+				//weightLabelLong.Text = String.Format("{0}+{1}", weighttosource, weighttoterminal);
+				weightLabelShort.Text = String.Format("{0}", weighttosource + weighttoterminal);
+				Calculated = true;
+			}
+		}
+	}
+}
